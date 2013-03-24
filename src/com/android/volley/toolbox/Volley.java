ac56add9c5f06522ff2054007516f08c2490dde8
Evan Charlton <evancharlton@gmail.com>
2013-03-24 12:32:47 -0700

    Create cache dir optimistically
    Create the cache folder when using the default cache setup. Without
    this, the cache will just silently fail to read and write.

--- src/com/android/volley/toolbox/Volley.java
+++ src/com/android/volley/toolbox/Volley.java
@@ -16,14 +16,14 @@
 
 package com.android.volley.toolbox;
 
+import com.android.volley.Network;
+import com.android.volley.RequestQueue;
+
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.http.AndroidHttpClient;
 import android.os.Build;
-
-import com.android.volley.Network;
-import com.android.volley.RequestQueue;
 
 import java.io.File;
 
@@ -33,13 +33,15 @@ public class Volley {
     private static final String DEFAULT_CACHE_DIR = "volley";
 
     /**
-     * Creates a default instance of the worker pool and calls {@link RequestQueue#start()} on it.
-     *
+     * Creates a default instance of the worker pool and calls
+     * {@link RequestQueue#start()} on it.
+     * 
      * @param context A {@link Context} to use for creating the cache dir.
      * @return A started {@link RequestQueue} instance.
      */
     public static RequestQueue newRequestQueue(Context context) {
         File cacheDir = new File(context.getCacheDir(), DEFAULT_CACHE_DIR);
+        cacheDir.mkdirs();
 
         String userAgent = "volley/0";
         try {
@@ -55,7 +57,8 @@ public class Volley {
             stack = new HurlStack();
         } else {
             // Prior to Gingerbread, HttpUrlConnection was unreliable.
-            // See: http://android-developers.blogspot.com/2011/09/androids-http-clients.html
+            // See:
+            // http://android-developers.blogspot.com/2011/09/androids-http-clients.html
             stack = new HttpClientStack(AndroidHttpClient.newInstance(userAgent));
         }
 
